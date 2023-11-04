84284bd50a37738a5f3bb569efbccf45d5f40a80
arrem4eg <arem4eg@gmail.com>
2023-11-04 21:03:06 +0100

    add homework14

--- src/test/java/homework9/Task3Test.java
+++ src/test/java/homework9/Task3Test.java
@@ -9,12 +9,12 @@ public class Task3Test {
     @Test
     public void rmString() {
 
-        String str1 = "abcd4efabc123deabcdaaa";
+        String str = "abcd4efabc123deabcdaaa";
         String[] removeChars = {"a", "d", "3"};
 
-        System.out.println("Initial string: " + str1);
+        System.out.println("Initial string: " + str);
         System.out.println("Characters to remove: " + Arrays.toString(removeChars));
-        String s = Task3.removeString(str1, removeChars);
+        String s = Task3.removeString(str, removeChars);
         System.out.println("Modified string: " + s);
 
     }
